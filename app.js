3cb072f7c2cd81220a8dcc1cfae3be67d499b257
AKIN-THOMAS <104861747+AKIN-THOMAS@users.noreply.github.com>
2023-09-07 04:31:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slackUserName = document.querySelector('[data-testid="slackUserName"]');
+  const currentDay = document.querySelector(
+    '[data-testid="currentDayOfTheWeek"]'
+  );
+  const utcTime = document.querySelector('[data-testid="currentUTCTime"]');
+
+  const getCurrentDayOfTheWeek = () => {
+    const daysOfTheWeek = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    const today = new Date();
+    console.log(daysOfTheWeek[today.getUTCDay()]);
+    return daysOfTheWeek[today.getUTCDay()];
+  };
+
+  const getUTCTime = () => {
+    const time = new Date();
+    return time.toUTCString();
+  };
+
+  currentDay.textContent = getCurrentDayOfTheWeek();
+  utcTime.textContent = getUTCTime();
+  slackUserName.textContent = "Akin-Thomas Bishop";
+});
